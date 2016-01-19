46a0c17dd5e0a8043e3552fea630243d461d21e1
dan <danpprince@gmail.com>
2016-01-18 23:43:07 -0500

    Maintain recently played queue
    To prevent recordings from playing too frequently, a recently played queue is maintained of length RECENTLY_PLAYED_LEN.

--- recorder.py
+++ recorder.py
@@ -3,6 +3,7 @@ from glob import glob
 import logging as log
 import numpy
 import os
+from os.path import basename
 import pyaudio
 from random import randint
 from scipy.io import wavfile
@@ -16,6 +17,14 @@ import wave
 BAUD         =  9600
 AUDIO_CHUNK  =  1024
 SAMPLE_RATE  = 44100
+
+# Set a constant size for the recently played queue in order to control
+# the number of played recordings that must occur before a single recording
+# should be played again
+RECENTLY_PLAYED_LEN = 5
+
+# Set the directory for recordings to be read from and written to
+REC_DIR = 'play/'
 
 # Define recording and playing states
 PLAYING   = 0
@@ -56,15 +65,23 @@ p = pyaudio.PyAudio()
 
 # Open the audio files to be played back
 log.info('Initializing audio queue')
-wavnames = glob('./play/*.wav')
+wavnames = [basename(f) for f in glob(REC_DIR + '*.wav')]
 log.debug('Recordings in queue: ' + str(wavnames))
 
 # Randomly select a file to use initially
 current_file = wavnames[randint(0, len(wavnames)-1)]
-rate, data = wavfile.read(current_file)
+rate, data = wavfile.read(REC_DIR + current_file)
 
 data_idx = 0
 log.info('Randomly selected ' + current_file + ' for first in queue')
+
+# Initialize queue lists
+recently_played_files = []
+up_next_files         = []
+
+# Maintain the recently played file queue
+recently_played_files.append(current_file)
+log.debug('File ' + current_file + ' appended to recently played queue')
 
 
 # Open stream for playing back recordings using blocking API
@@ -119,8 +136,6 @@ else:
 
 rec_frames = []
 
-up_next_files         = []
-
 log.info('Application started! ^_^')
 
 while True:
@@ -153,8 +168,8 @@ while True:
 
                 # Save record data in a new wave file with the current timestamp
                 timestamp_str = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-                new_fname = './play/rec_' + timestamp_str + '.wav'
-                log.info('Saving new recording ' + new_fname)
+                new_fname = REC_DIR + 'rec_' + timestamp_str + '.wav'
+                log.info('Saving new recording ' + basename(new_fname))
                 wf = wave.open(new_fname, 'wb')
                 wf.setnchannels(1)
                 wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
@@ -163,10 +178,10 @@ while True:
                 wf.close()
 
                 # Play this newly recorded file next
-                up_next_files.append(new_fname)
+                up_next_files.append(basename(new_fname))
 
                 # Get the new list of wavfiles 
-                wavnames = glob('./play/*.wav')
+                wavnames = [basename(f) for f in glob(REC_DIR + '*.wav')]
 
                 rec_frames = []
 
@@ -186,18 +201,34 @@ while True:
 
         # If the end of the current wavfile is reached, play a new file
         if data_idx >= len(data):
-            # Select a new file to play next that is not the same as the current file
+            log.debug('File ' + current_file + ' finished playing')
+
+            # If a newly recorded file has been created and added to the up next
+            # queue, play it now
             if len(up_next_files) > 0:
-                new_file = up_next_files.pop(0)
                 log.debug('Newly recorded files in queue ' + str(up_next_files))
+                new_file = up_next_files.pop(0)
                 log.info('Playing newly recorded file ' + new_file)
+
+            # If nothing in up next queue, select a new file that has not
+            # been played recently
             else:
                 new_wav_candidates = [f for f in wavnames if f not in recently_played_files]
                 log.debug('Next recording candidates: ' + str(new_wav_candidates))
                 new_file = new_wav_candidates[randint(0, len(new_wav_candidates)-1)]
                 log.info('Playing ' + new_file)
 
-            new_rate, new_data = wavfile.read(new_file)
+            # Maintain the recently played file queue
+            recently_played_files.append(new_file)
+            log.debug('File ' + new_file + ' appended to recently played queue')
+
+            if len(recently_played_files) > RECENTLY_PLAYED_LEN:
+                least_recent_file = recently_played_files.pop(0)
+                log.debug('File ' + least_recent_file + ' removed from recently played queue')
+
+            log.debug('New recently played files queue: ' + str(recently_played_files))
+
+            new_rate, new_data = wavfile.read(REC_DIR + new_file)
 
             current_file = new_file
             data         = new_data
